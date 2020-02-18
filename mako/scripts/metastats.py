34ab6d83095ba660a1be848735aece301764d96a
ramellose <lisa.rottjers@kuleuven.be>
2020-02-18 09:46:57 +0100

    bugfix

--- mako/scripts/metastats.py
+++ mako/scripts/metastats.py
@@ -454,10 +454,10 @@ class MetastatsDriver(ParentDriver):
             old_links.append(item['p'].nodes[1].get('name'))
         for item in old2:
             old_links.append(item['p'].nodes[1].get('name'))
-        tx.run(("MATCH p=(a)-[r:WITH_TAXON]-(:Edge)-(:Network {name: '" + network +
+        tx.run(("MATCH p=(a)-[r:WITH_TAXON]-(:Edge)--(:Network {name: '" + network +
                 "'}) WHERE a.name = '" +
                 path.nodes[1].get('name') + "' DELETE r"))
-        tx.run(("MATCH p=(a)-[r:WITH_TAXON]-(:Edge)-(:Network {name: '" + network +
+        tx.run(("MATCH p=(a)-[r:WITH_TAXON]-(:Edge)--(:Network {name: '" + network +
                 "'}) WHERE a.name = '" +
                 path.nodes[5].get('name') + "' DELETE r"))
         old_links = list(set(old_links))  # issue with self loops causing deletion issues
