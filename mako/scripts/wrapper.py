bb48a556b260b139b8ff5e27db2eae79e83f042e
ramellose <lisa.rottjers@kuleuven.be>
2020-03-12 16:42:04 +0100

    updated wrapper bits

--- mako/scripts/wrapper.py
+++ mako/scripts/wrapper.py
@@ -12,6 +12,9 @@ __license__ = 'Apache 2.0'
 
 import sys
 from mako.scripts.io import IoDriver
+from manta.cluster import cluster_graph
+from manta.reliability import perm_clusters
+from anuran.main import model_calcs
 from mako.scripts.utils import ParentDriver, _get_unique, _create_logger, _read_config
 import logging.handlers
 
@@ -57,12 +60,55 @@ def start_wrapper(inputs):
         logger.error("Login information not specified in arguments.", exc_info=True)
         exit()
     # get networks
-
+    networks = driver.return_networks(inputs['networks'])
     # run manta
-
+    if 'manta' in inputs:
+        for network in networks:
+            run_manta(inputs, network, driver)
+    if 'anuran' in inputs:
+        run_anuran(inputs, networks, driver)
     # run anuran
 
     # add properties to database
 
     driver.close()
     logger.info('Completed netstats operations!  ')
+
+
+def run_manta(inputs, network, driver):
+    """
+    Takes the extracted network object and runs manta.
+    The manta results are then uploaded back to
+    the database as node properties.
+    :param inputs: Arguments for manta
+    :param network: Network to cluster
+    :param driver: Neo4j IO driver
+    :return:
+    """
+    results = cluster_graph(network, limit=inputs['limit'], max_clusters=inputs['max'],
+                            min_clusters=inputs['min'], min_cluster_size=inputs['ms'],
+                            iterations=inputs['iter'], subset=inputs['subset'],
+                            ratio=inputs['ratio'], edgescale=inputs['edgescale'],
+                            permutations=inputs['perm'], verbose=True)
+    graph = results[0]
+    if inputs['cr']:
+        perm_clusters(graph=graph, limit=inputs['limit'], max_clusters=inputs['max'],
+                      min_clusters=inputs['min'], min_cluster_size=inputs['ms'],
+                      iterations=inputs['iter'], ratio=inputs['ratio'],
+                      partialperms=inputs['perm'], relperms=inputs['rel'], subset=inputs['subset'],
+                      error=inputs['error'], verbose=True)
+    # do something with writing to the database here
+
+
+def run_anuran(inputs, networks, driver):
+    """
+    Takes the extracted network objects and runs anuran.
+    The anuran centrality results are then uploaded back to
+    the database as node properties.
+    :param inputs: Arguments for anuran
+    :param network: Network to cluster
+    :param driver: Neo4j IO driver
+    :return:
+    """
+    centralities = model_calcs(networks=networks, inputs=inputs)
+    # do something with writing to the database here
